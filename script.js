7dd324f5bb39694ebdee225706fd003c1019ad0a
Riwaj Rai <riwajrai83@gmail.com>
2024-07-01 12:33:46 -0700

    First Commit: added EventHandler and logics

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+console.log(document.queryselector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
